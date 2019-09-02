7cfb9fb85db5289237b882e526ef1ca9a6b50c89
anbya <anbyaalibia@gmail.com>
2019-09-02 14:33:55 +0700

    a

--- index.js
+++ index.js
@@ -41,3 +41,29 @@ alert(hero)
 alert(hero['name'])
 alert(hero.age)
 alert(hero.superPower)
+
+console.log([1, 2, 3])
+console.log([0, 1, 2, 3])
+console.log(['Alpha', 100, true])
+
+const person = ['Alpha', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person[0])
+alert(person[1])
+alert(person[2])
+
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = false
+
+console.log(`The lamp's toggle is ${toggle}`)
+
+alert(`The lamp's toggle is ${toggle}`)
