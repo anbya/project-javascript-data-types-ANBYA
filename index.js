bb0cd8f144664df4e18691e85bb702e6d920d3e9
anbya <anbyaalibia@gmail.com>
2019-09-02 14:32:36 +0700

    a

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-var name = "anbya"
-console.log(name)
+console.log('Hello World')
+console.warn('Hello Warning')
+console.error('Hello Error')
+console.info('Hello Info')
+
+alert('Hello Alert')
+
+console.log('Hello World')
+
+const name = 'ANBYA ARMY ALI'
+
+console.log(`Hello ${name}`)
+
+alert(`Hello ${name}`)
+
+console.log(0)
+console.log(100)
+console.log(3.14)
+
+const age = 28
+
+console.log(`His age is ${age}`)
+
+alert(`His age is ${age}`)
+
+console.log({ name: 'ANBYA', age: 28, superPower: true })
+console.log({
+  name: 'ANBYA',
+  age: 28,
+  superPower: true
+})
+
+const hero = { name: 'ANBYA', age: 28, superPower: true }
+
+console.log(hero)
+console.log(hero['name'])
+console.log(hero.age)
+console.log(hero.superPower)
+
+alert(hero)
+alert(hero['name'])
+alert(hero.age)
+alert(hero.superPower)
